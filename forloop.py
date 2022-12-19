d4d6bd1feb3c376a888496a7bc7a2fc1b2d26883
nikhilpaspuleti20 <116423567+nikhilpaspuleti20@users.noreply.github.com>
2022-12-19 20:44:31 +0530

    Add files via upload

---
+++ forloop.py
@@ -0,0 +1,12 @@
+""""
+DATE:11TH NOV 2022
+DATE:11TH DEC 2022
+DAY: SUNDAY
+TOPIC: FOR LOOP
+AUTHOR: nikhil
+"""
+    # print(i) # 1 2 3 4 (vertically)   
+n=int(input('enter the value of n:'))
+for i in range(1,n+1):
+    print(i,end=" ")    # 1 2 3 4 5
+    print(i,end=" ")    # 1 2 3 4 5
---
+++ loop1.py
@@ -0,0 +1,12 @@
+"""
+DATE:11TH NOV 2022
+DATE:11TH DEC 2022
+DAY: SUNDAY
+TOPIC: FOR LOOP
+AUTHOR: POOJA
+"""
+for i in range(10,0,-1):
+    print(i,end=" ")    # 10  9  8 7 4 6 5 4 3 2 1
+for i in range(10,-1,-1):
+    print(i,end=' ')    # 10 9 8 7 6 5 4 3 2 1 0
+    print(i,end=' ')    # 10 9 8 7 6 5 4 3 2 1 0
